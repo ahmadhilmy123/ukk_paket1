826563459cbd0bf57fbd737b9034e693c59ab0d7
ahmadhilmy123 <tomohilmy@gmail.com>
2023-02-17 09:37:42 +0700

    comit

---
+++ resources/views/auth/siswa-login.blade.php
@@ -0,0 +1,66 @@
+@extends('layouts.auth')
+
+@section('title', 'Login')
+
+@push('style')
+    <!-- CSS Libraries -->
+    <link rel="stylesheet"
+        href="{{ asset('library/bootstrap-social/bootstrap-social.css') }}">
+@endpush
+
+@section('main')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-19">
+            <div class="card">
+                <div class="card-header">{{ __('Login Siswa') }}</div>
+                     <div class="alert alert-success m-3">
+                        Login untuk <b>petugas</b> silahkan <a href="{{ url('login') }}" class="text-primary" style="text-decoration:none;">Klik Disini</a>
+                     </div>
+                <div class="card-body">
+                    <form method="POST" action="{{ url('login/siswa/proses') }}">
+                        @csrf
+
+                        <div class="form-group row">
+                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('NISN') }}</label>
+
+                            <div class="col-md-8">
+                                <input type="text" class="form-control @error('nisn') is-invalid @enderror" name="nisn" value="{{ old('nisn') }}" required autofocus>
+
+                                @error('nisn')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>
+
+                        <div class="form-group row">
+                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Nama Lengkap') }}</label>
+
+                            <div class="col-md-8">
+                                <input id="nama_siswa" type="text" class="form-control @error('nama_siswa') is-invalid @enderror" name="nama_siswa" value="{{ old('nisn') }}" required>
+
+                                @error('nama_siswa')
+                                    <span class="invalid-feedback" role="alert">
+                                        <strong>{{ $message }}</strong>
+                                    </span>
+                                @enderror
+                            </div>
+                        </div>                        
+
+                        <div class="form-group row mb-0">
+                            <div class="col-md-8 offset-md-4">
+                                <button type="submit" class="btn btn-primary">
+                                    {{ __('Login') }}
+                                </button>
+
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
---
+++ resources/views/components/sidebar.blade.php
@@ -0,0 +1,62 @@
+<div class="main-sidebar sidebar-style-2">
+    <aside id="sidebar-wrapper">
+        <div class="sidebar-brand">
+            <a href="index.html">Stisla</a>
+        </div>
+        <div class="sidebar-brand sidebar-brand-sm">
+            <a href="index.html">St</a>
+        </div>
+        <ul class="sidebar-menu">
+            <li class="sidebar-item"> <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard') }}" aria-expanded="false"><i class="mdi mdi-view-dashboard"></i><span class="hide-menu">Dashboard</span></a></li>
+					{{-- @if(auth()->user()->level == 'admin') --}}
+                        		<li class="sidebar-item">
+							<a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/data-siswa') }}" aria-expanded="false">
+								<i class="fa fa-user fa-1x"></i>
+								<span class="hide-menu">Data Siswa</span>
+							</a>
+						</li>
+                        		<li class="sidebar-item">
+							<a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/data-petugas') }}" aria-expanded="false">
+								<i class="fa fa-user-circle fa-1x"></i>
+								<span class="hide-menu">Data Petugas</span>
+							</a>
+						</li>
+                        		<li class="sidebar-item">
+							 <a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/data-kelas') }}" aria-expanded="false">
+								<i class="mdi mdi-home-variant"></i>
+									<span class="hide-menu">Data Kelas</span>
+							</a>
+						</li>
+						<li class="sidebar-item">
+							<a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/data-spp') }}" aria-expanded="false">
+								<i class="mdi mdi-cash-usd"></i>
+									<span class="hide-menu">Data SPP</span>
+							</a>
+						</li>
+                              {{-- @endif
+                              @if(auth()->user()->level == 'admin' || auth()->user()->level == 'petugas') --}}
+						<li class="sidebar-item">
+							<a class="sidebar-liauth()->user()->level == 'admin'nk waves-effect waves-dark sidebar-link" href="{{ url('dashboard/pembayaran') }}" aria-expanded="false">
+								<i class="mdi mdi-cash"></i>
+									<span class="hide-menu">Entri Transaksi Pembayaran</span>
+							</a>
+						</li>
+                              {{-- @endif                        --}}
+						<li class="sidebar-item">
+							<a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/histori') }}" aria-expanded="false">
+								<i class="mdi mdi-note-multiple"></i>
+									<span class="hide-menu">History Pembayaran</span>
+							</a>
+						</li>
+                              {{-- @if(auth()->user()->level == 'admin') --}}
+						<li class="sidebar-item">
+							<a class="sidebar-link waves-effect waves-dark sidebar-link" href="{{ url('dashboard/laporan') }}" aria-expanded="false">
+								<i class="mdi mdi-file-document"></i>
+									<span class="hide-menu">Generate Laporan</span>
+							</a>
+					     </li>
+				         {{-- @endif   --}}
+                    </ul>
+                    
+    </aside>
+</div>
